6ce1dac1a72e2a65ebe8eb09a3981926ab2fdbd4
KiichiroYamano <101058464kiichiro@gmail.com>
2020-07-09 22:46:19 +0900

    commit ALDS_01_A_InsertionSort

---
+++ ALDS_01_A_InsertionSort/ALDS_01_A_InsertionSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void out_int_array(int A[], int n);
+
+int main()
+{
+    int n,A[100]={};
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    out_int_array(A, n);
+    int v,j;
+    for (int i = 1; i < n; i++)
+    {
+        v = A[i];
+        j = i - 1;
+        while(j >= 0 && A[j] >v)
+        {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j + 1] = v;
+        out_int_array(A, n);
+    }
+    
+    return 0;
+}
+
+void out_int_array(int A[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << A[n -1] << endl;
+}

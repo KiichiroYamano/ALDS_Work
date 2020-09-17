a52549e6439c04f7bc9e93964c2c7939db30964f
KiichiroYamano <101058464kiichiro@gmail.com>
2020-09-17 11:59:48 +0900

    commit ALDS_07_A_RootedTree

--- ALDS_07_A_RootedTree/ALDS_07_A_RootedTree.cpp
+++ ALDS_07_A_RootedTree/ALDS_07_A_RootedTree.cpp
@@ -10,6 +10,7 @@ struct Node tree[100000];
 void generateTree(int p, int rs);
 void setNode(int p, int l, int r, int id);
 int getDepth(int id);
+void generateTree2(int n);
 
 void setNode(int p, int l, int r, int id)
 {
@@ -43,6 +44,35 @@ void generateTree(int p, int rs)
     {
         setNode(p, NIL, rs, id);   
     }
+}
+
+void generateTree2(int n)
+{
+    int id, k;
+    vector<int> children;
+    
+    for(int m =0; m < n; m++)
+    {
+        scanf("%d%d", &id, &k);
+        int tmp;
+        for(int i =0;i < k;i++)
+        {
+            scanf("%d", &tmp);
+            children.push_back(tmp);
+            if(i == 0)
+            {
+                tree[id].leftChild = tmp;
+                tree[tmp].parent = id;
+            }
+            if(i >= 1)
+            {
+                tree[tmp].parent = id;
+                tree[children[i - 1]].rightSibling = tmp;
+            }
+        }
+        children.clear();
+    }
+
 }
 
 int getDepth(int id)
@@ -84,8 +114,9 @@ int main()
     int n;
 
     scanf("%d", &n);
+    for(int i=0; i < n; i++) setNode(NIL,NIL,NIL,i);
 
-    generateTree(NIL,NIL);
+    generateTree2(n);
 
     outputTree(n);
     
